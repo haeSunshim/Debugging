307952c730c8f375dc5711a4ef1cfe220781e500
yyyhang <yyuhang.it@gmail.com>
2020-07-12 02:14:14 +1000

    init

--- debugging/src/demo.py
+++ debugging/src/demo.py
@@ -1,10 +1,42 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask import render_template
 
 app = Flask(__name__)
 
+class Config(object):
+    DEBUG = True
+    ITCAST = "python"
+
+app.config.from_object(Config)
+
+db = SQLAlchemy(app)
+
+class Host(db.Model):
+    __tablename__ = "tbl_hosts"
+
+    id = db.column(db.Integer, primary_key = True)
+    name = db.Column(db.String(32))
+    refugee =
+
+
+
+
+
+# the main page
 @app.route("/")
-def index():
+def homepage():
+    return app.send_static_file('homepage.html')
+
+@app.route("/signin/")
+def signIn():
+    return
+
+@app.route("/register/", methods=["GET","POST"])
+def register():
+
+
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='127.0.0.1',port=5000)
